58c044fe18259ffd55d82d640f68195eb9c867d0
MHibriziF <hibrizifarghana@gmail.com>
2024-08-28 14:20:32 +0700

    Add: PWS Deployment

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9+=7tr%cdfd8mfciuk@a*fhz($xhblkd*o_v8&2ec$$f3a(#l@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-hibrizi-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 # Application definition
 
